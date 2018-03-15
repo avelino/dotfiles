f2ae6361cd27f13d36d9d81626c3fbcb7e1c7a9f
Avelino <t@avelino.xxx>
2018-03-15 17:51:12 -0400

    upgrade debian package/server

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ## install packages for debian
 sudo apt-key add debian.repokeys
 sudo cp debian.sources /etc/apt/sources.list
-sudo apt update && sudo apt upgrade && sudo apt-get install dselect
+sudo apt update && sudo apt upgrade && sudo apt-get install dselect && sudo dselect update && sudo apt --fix-broken install
 sudo dpkg --set-selections < debian.packages
 sudo apt-get dselect-upgrade -y
 
