438672730df41be51406360536789c077ee4ea0f
Avelino <t@avelino.xxx>
2019-08-02 07:45:07 -0300

    create emacs alias start new deamon by tmux session

--- zsh_custom/plugins/my-aliases/my-aliases.plugin.zsh
+++ zsh_custom/plugins/my-aliases/my-aliases.plugin.zsh
@@ -1,10 +1,54 @@
 alias g=git
 alias G=g
-alias e="emacsclient -nw --socket-name avelino"
-alias vi="emacsclient -nw --socket-name avelino"
-alias es="emacs --eval '(setq server-name \"avelino\")' --daemon"
-alias ex="emacsclient -cnfs --socket-name avelino"
-alias esk="killall emacs"
+alias e=emacs_open
+alias vi=e
+alias es=emacs_start
+# alias es="emacs --eval '(setq server-name \"avelino\")' --daemon"
+alias ex=emacsx_start
+# alias ex="emacsclient -cnfs --socket-name avelino"
+alias esk=emacs_kill
+# alias esk="killall emacs"
 alias emacs="emacs -nw"
 alias htop="TERM=screen htop"
 alias gl="git log --graph --pretty='format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
+alias loadrbenv='rbenv init -'
+alias loadrvm='[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"'
+alias loadnvm='[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" && [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"'
+alias loadpyenv='pyenv init -'
+
+alias loadtravis='[ -f $HOME/.travis/travis.sh ] && . "$HOME/.travis/travis.sh"'
+
+function emacs_session_name(){
+    TMUX_SESSION=$(tmux display-message -p '#S')
+    SESSION="${TMUX_SESSION}-${PWD##*/}"
+}
+
+function emacs_start(){
+    emacs_session_name
+    emacs --eval "(setq server-name \"$SESSION\")" --daemon
+}
+
+function emacsx_start(){
+    emacs_session_name
+    emacsclient -cnfs --socket-name $SESSION
+}
+
+function emacs_kill(){
+    emacs_session_name
+    killall emacs $SESSION
+}
+
+function emacs_open(){
+    emacs_session_name
+    {
+	emacsclient -nw --socket-name $SESSION $1
+    } || {
+	emacs_start
+	emacsclient -nw --socket-name $SESSION $1
+    }
+}
+
+open_project() {
+    cd ~/src/github.com/$1
+}
+alias oproject=open_project
