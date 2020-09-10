8cd7a64e07c93e737e445f1b853d48ec883b5664
Avelino <t@avelino.xxx>
2020-09-10 15:32:22 -0300

    upgrade brew dump file
    Signed-off-by: Avelino <t@avelino.xxx>

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ mkdir -p ~/.config/nvim && \
 ./dotfiles/golang.sh
 
 ## zsh plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
