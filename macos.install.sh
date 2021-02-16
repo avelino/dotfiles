bda28667eae8fb482f9405183aca7ba92f96db01
Avelino <t@avelino.xxx>
2021-02-16 10:54:03 -0300

    set location screencpture
    Signed-off-by: Avelino <t@avelino.xxx>

--- macos.install.sh
+++ macos.install.sh
@@ -208,7 +208,7 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+defaults write com.apple.screencapture location -string "${HOME}/Google\ Drive/My\ Drive/Screenshots/"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
