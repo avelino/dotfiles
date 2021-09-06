b032e5837261c88568334bd066275c0f1e02505e
Avelino <avelinorun@gmail.com>
2021-09-06 18:41:11 -0300

    organize install file
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 brew bundle
 
+## oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 ## copy files
 cp ~/dotfiles/gitconfig ~/.gitconfig
 cp ~/dotfiles/tmux.conf ~/.tmux.conf
-cp ~/dotfiles/zshrc ~/.zshrc
+rm ~/.zshrc && \
+    cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/hyper.js ~/.hyper.js
 cp ~/dotfiles/fzf.zsh ~/.fzf.zsh
 cp ~/dotfiles/gitignore_global ~/.gitignore_global
@@ -21,7 +25,9 @@ git clone git://github.com/avelino/.emacs.git ~/.emacs.d
 
 ## vim
 mkdir -p ~/.config/nvim && \
-    curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=typescript&langs=html&langs=python&langs=go&langs=lua&theme=dracula&editor=nvim' > ~/.config/nvim/init.vim
+    curl -X POST 'https://vim-bootstrap.com/generate.vim' \
+    --data 'langs=javascript&langs=typescript&langs=html&langs=python&langs=go&langs=lua&theme=dracula&editor=nvim' \
+    > ~/.config/nvim/init.vim
 
 ## golang install bins
 ./dotfiles/golang.sh
@@ -30,6 +36,3 @@ mkdir -p ~/.config/nvim && \
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-## oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
