fbd50b29131f91e936b7ccc7a9dc30540f7d34e5
Avelino <avelinorun@gmail.com>
2022-10-08 17:07:39 -0300

    fish: keychain and gpg agent load
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-source ~/.config/fish/aliases.fish
-set -gx GPG_TTY (tty)
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source ~/.config/fish/aliases.fish
+    set -gx GPG_TTY (tty)
+    keychain --agents ssh --eval id_rsa | source
 end
