d2d2fda3fd661f3b8be17da8871bf2edf2e2e16d
Avelino <avelinorun@gmail.com>
2022-11-03 15:40:20 -0300

    emacs/+lsp: active in clojure and python
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- doom.d/init.el
+++ doom.d/init.el
@@ -116,7 +116,7 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
-       clojure             ; java with a lisp
+       (clojure +lsp)             ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -157,7 +157,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python              ; beautiful is better than ugly
+       (python +lsp)              ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        racket              ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
