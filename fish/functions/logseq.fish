99881a0763423d0540d3122a4f9ecb2057a3efc0
Avelino <avelinorun@gmail.com>
2022-12-21 12:19:18 -0300

    logseq sync: push before commit
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- fish/functions/logseq.fish
+++ fish/functions/logseq.fish
@@ -1,10 +1,8 @@
 function logseq --description 'logseq shortcut with new functions'
-  switch $argv[1]
-    case sync
-      cd ~/Google\ Drive/My\ Drive/logseq/avelino && \
-         g add . && \
-         g ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq";
-    case '*' # default
-      echo "logseq method not fund";
-  end
+    switch $argv[1]
+        case sync
+            cd ~/Google\ Drive/My\ Drive/logseq/avelino && g add . && g ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq" && g push
+        case '*' # default
+            echo "logseq method not fund"
+    end
 end
