123c8693391ca2c0208093020750127fd6946de2
Avelino <avelinorun@gmail.com>
2023-07-30 15:26:41 -0300

    fish/emacs: change alias e to emacsclient
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,4 +1,6 @@
 # basically it checks whether we have a running emacs server; if we don't, it starts one
 function e
-    emacs -nw $argv
+    # use emacsclient if we have a running server
+    # emacs -nw $argv
+    emacsclient -c $argv
 end
