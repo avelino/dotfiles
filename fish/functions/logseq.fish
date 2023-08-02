8195e8706a4200fd888fb25575dc742b1af7787b
Avelino <avelinorun@gmail.com>
2023-08-02 11:32:32 -0300

    fish/logseq: improved synchronization
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- fish/functions/logseq.fish
+++ fish/functions/logseq.fish
@@ -1,12 +1,10 @@
 function logseq --description 'logseq shortcut with new functions'
     switch $argv[1]
         case sync
-            rsync -aP --exclude=.git ~/logseq/* ~/logseq.git/ && \
-                cd ~/logseq.git && \
-                g add . && \
-                g ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq" && \
-                g push && \
-                cd -
+            rsync -aP -q --exclude=.git \
+                --exclude="logseq/*/" \
+                --exclude="logseq/.recycle/" \
+                ~/logseq/* ~/logseq.git/ && cd ~/logseq.git && g st && g add . && g ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq" && g push && cd -
         case '*' # default
             echo "logseq method not fund"
     end
