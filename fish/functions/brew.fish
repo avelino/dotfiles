7c3f5e232c1ab1d13d63a88e4b09e9ac05cf3da1
Avelino <31996+avelino@users.noreply.github.com>
2024-04-18 17:20:05 -0300

    fish/brew: clear cache folder
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -1,9 +1,18 @@
 function brew --description 'brew shortcut with new functions'
     switch $argv[1]
         case bkp
-            rm ~/dotfiles/Brewfile && cd ~/dotfiles && brew bundle dump --force && defaults export NSGlobalDomain ~/dotfiles/macos/nsglobaldomain.plist
+            rm ~/dotfiles/Brewfile && \
+            cd ~/dotfiles && \
+            brew bundle dump --force && \
+            defaults export NSGlobalDomain ~/dotfiles/macos/nsglobaldomain.plist
         case up
-            brew update && brew upgrade && brew cu -ay --cleanup && brew clean && brew cleanup
+            brew update && \
+            brew upgrade && \
+            brew cu -ay --cleanup && \
+            brew clean && \
+            brew cleanup && \
+            rm -rf $HOME/Library/Caches/Homebrew && \
+            rm -rf $HOME/Library/Logs/Homebrew
         case clean
             brew cleanup --prune=all -s -n
         case emacs
