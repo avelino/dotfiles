a4b56362bb140762c650e6f2c1712333131ee21d
Avelino <31996+avelino@users.noreply.github.com>
2024-04-30 09:40:41 -0300

    mvim/color: fixed background in term
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,31 @@
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  opts =  {
-    dark_variant = "dawn",
+  opts = {
+    variant = "auto",
+    dark_variant = "auto",
     extend_background_behind_borders = true,
+    disable_background = true,
     enable = {
       terminal = true,
       legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
       migrations = true, -- Handle deprecated options automatically
     },
+    styles = {
+      transparency = false,
+    },
   },
   config = function(_, opts)
     require("rose-pine").setup(opts)
     vim.cmd.colorscheme("rose-pine-dawn")
-  end
+    ColorMyPencils()
+  end,
 }
