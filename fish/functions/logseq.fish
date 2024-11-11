ce18406b878286eb285f5f70870556bcb911cd45
Avelino <31996+avelino@users.noreply.github.com>
2024-11-10 23:09:47 -0300

    fish/logseq: change default folder
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- fish/functions/logseq.fish
+++ fish/functions/logseq.fish
@@ -2,15 +2,20 @@ function logseq --description 'logseq shortcut with new functions'
     switch $argv[1]
         case sync
             echo "logseq: sync with git..."
-            logseq icloud
+            logseq cloud
             cd ~/notes && g pull -f
             rsync -aP -q --exclude=.git --delete \
                 --exclude="logseq/*/" \
                 --exclude="logseq/.recycle/" \
-                ~/logseq/* ~/notes/ && g st && g add . && g ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq" && g push && cd -
-        case icloud
-            echo "logseq: icloud sync..."
-            cat ~/logseq/journals/$(date +%Y-%m-%d).md 1>/dev/null
+                ~/logseq-avelino/* ~/notes/ && \
+            git st && \
+            git add . && \
+            git ci -am "$(date '+%Y-%m-%d %H:%M'): sync logseq" && \
+            git push && \
+            cd -
+        case cloud
+            echo "logseq: journal day init..."
+            cat ~/logseq-avelino/journals/$(date +%Y-%m-%d).md 1>/dev/null
         case '*' # default
             echo "logseq: method not fund"
     end
