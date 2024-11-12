c29f0bc3836fbf6814a04ab0dfe177149286790e
Avelino <thiago.avelino@buser.com.br>
2024-11-12 13:32:27 -0300

    brew sync
    Signed-off-by: Avelino <thiago.avelino@buser.com.br>

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -1,9 +1,19 @@
 function brew --description 'brew shortcut with new functions'
     switch $argv[1]
         case bkp
-            rm ~/dotfiles/Brewfile && cd ~/dotfiles && brew bundle dump --force && defaults export NSGlobalDomain ~/dotfiles/macos/nsglobaldomain.plist
+            rm ~/dotfiles/Brewfile && \
+            cd ~/dotfiles && \
+            brew bundle dump --force && \
+            defaults export NSGlobalDomain ~/dotfiles/macos/nsglobaldomain.plist
         case up
-            brew update && brew upgrade && brew cu -ay --cleanup && brew clean && brew cleanup && rm -rf $HOME/Library/Caches/Homebrew && rm -rf $HOME/Library/Logs/Homebrew && cd -
+            brew update && \
+            brew upgrade && \
+            brew cu -ay --cleanup && \
+            brew clean && \
+            brew cleanup && \
+            rm -rf $HOME/Library/Caches/Homebrew && \
+            rm -rf $HOME/Library/Logs/Homebrew && \
+            cd -
         case clean
             brew cleanup --prune=all -s -n
         case emacs
@@ -12,6 +22,12 @@ function brew --description 'brew shortcut with new functions'
             skhd --$argv[2]-service
         case yabai
             yabai --$argv[2]-service
+        case spacebar
+            brew services $argv[2] cmacrae/formulae/spacebar
+        case i3
+            brew yabai $argv[2] && \
+            brew skhd $argv[2] && \
+            brew spacebar $argv[2]
         case '*' # default
             command brew $argv
     end
