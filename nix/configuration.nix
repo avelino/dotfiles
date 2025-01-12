c38864968a6aa776da43b6d6b2b5a192c7794224
Avelino <31996+avelino@users.noreply.github.com>
2025-01-12 09:05:19 -0300

    feat(nix): add nix-casks and fix lint
    - Add nix-casks to flake.nix
    - Fix lint in configuration.nix
    - Add fish shell support
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -26,21 +26,18 @@
     morgen
     rio
     orbstack
-  ])
+  ]);
   # Necessary for using flakes on this system.
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = "nix-command flakes";
 
   # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null
+  programs.fish.enable = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 5
+  system.stateVersion = 5;
   # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin"
+  nixpkgs.hostPlatform = "aarch64-darwin";
   # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.fish.enable = true;
+  services.nix-daemon.enable = true;
 } 
--- nix/flake.nix
+++ nix/flake.nix
@@ -5,19 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-casks = {
-      url = "github:atahanyorganci/nix-casks/archive";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-casks.url = "github:atahanyorganci/nix-casks/archive";
+    nix-casks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nix-darwin, nixpkgs }: {
+  outputs = { self, nix-darwin, nixpkgs, nix-casks }: {
     darwinConfigurations."Avelinos-Buser-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";  # Para M1/M2 Macs
       # Use "x86_64-darwin" para Intel Macs
       
       modules = [
         ./configuration.nix
+        {
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+        }
       ];
     };
   };
