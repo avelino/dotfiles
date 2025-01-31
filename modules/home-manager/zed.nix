803053339171548c3f7b759715fa90782a88d467
Avelino <31996+avelino@users.noreply.github.com>
2025-01-31 20:30:06 -0300

    zed: use xdg directory to save config
    - Changes nixfmt classic to default format
    - Fixed vim mode disable default value
    - Terminal: use fish shell with -l argument (load config).
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -1,45 +1,46 @@
 { pkgs, ... }:
 {
-  home.file.".config/zed/settings.json".text = builtins.toJSON ({
-    vim_mode = true;
-    autosave = "on_focus_change";
-    ui_font_size = 16;
-    buffer_font_size = 16;
-    theme = {
-      mode = "dark";
-      light = "Tokio Night Light";
-      dark = "Tokio Night";
-    };
-    assistant = {
-      default_model = {
-        provider = "anthropic";
-        model = "claude-3-5-sonnet-20240620";
+  xdg.configFile."zed/settings.json" = {
+    text = builtins.toJSON ({
+      vim_mode = false;
+      autosave = "on_focus_change";
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "dark";
+        light = "Tokyo Night Light";
+        dark = "Tokyo Night";
       };
-      # provider = "anthropic";
-      version = "1";
-      dock = "right";
+      assistant = {
+        default_model = {
+          provider = "anthropic";
+          model = "claude-3-5-sonnet-20240620";
+        };
+        provider = {
+          name = "anthropic";
+          default_model = "claude-3-5-sonnet";
+        };
+        version = "1";
+        dock = "right";
+        enabled = true;
+      };
+      auto_indent = true;
       enabled = true;
-    };
-    auto_indent = true;
-    enabled = true;
-    show_on_startup = true;
-    default_model = {
-      model = "en_core_web_sm";
-      provider = "zed.dev";
-    };
-    enable_experimental_live_diffs = true;
-    button = true;
-    terminal = {
-      # shell = "${pkgs.fish}/bin/fish";
-      # shell = "system";
-      # shell_args = [ "-l" ];
-      shell_env = {
-        LANG = "en_US.UTF-8";
-        LC_ALL = "en_US.UTF-8";
+      show_on_startup = true;
+      default_model = {
+        model = "en_core_web_sm";
+        provider = "zed.dev";
       };
-    };
-    formatter = {
-      nix = "nixfmt-classic";
-    };
-  });
+      enable_experimental_live_diffs = true;
+      button = true;
+      terminal = {
+        shell = "${pkgs.fish}/bin/fish";
+        shell_args = ["-l"];
+        shell_env = {
+          LANG = "en_US.UTF-8";
+          LC_ALL = "en_US.UTF-8";
+        };
+      };
+    });
+  };
 }
