e86b8f008ee8c4869f03d2bd84a2f531cdfc3753
Avelino <31996+avelino@users.noreply.github.com>
2025-02-08 21:38:15 -0300

    nix: add garbage collector and extra options
    - Add garbage collector configuration to run weekly and clean old files
    - Set trusted users for nix operations
    - Configure max jobs and cores for builds
    - Add essential nix options for better build management
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,7 +6,22 @@
       extra-experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
       accept-flake-config = true;
+      sandbox = false; # Temporarily disable sandbox for macOS builds
+      trusted-users = [ "root" "@admin" "@wheel" ];
+      max-jobs = "auto";
+      cores = 0;
     };
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 0; Minute = 0; }; # Run at midnight on Sundays
+      options = "--delete-older-than 30d";
+    };
+    extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+      experimental-features = nix-command flakes
+      build-users-group = nixbld
+    '';
   };
 
   nixpkgs = {
